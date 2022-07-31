300156c567bda4f3797371c912eac4850467e8fb
Chengsen Dong <1034029664@qq.com>
2022-07-31 13:44:27 +0100

    update padding operator

--- src/operator/opennna_operator_padding.c
+++ src/operator/opennna_operator_padding.c
@@ -14,10 +14,7 @@ void OpenNNA_Operator_Padding(struct layer *Layers)
     reg_t Output_Fmap_Col = ((Layer_Para_Base *)Layers->Layer_Para_Base)->Output_Fmap_Col;
 
     //Padding特有参数
-    reg_t Padding_Top = ((Layer_Para_Padding *)Layers->Layer_Para_Extra)->padding_top;//顶部填充行
-    reg_t Padding_Bottom = ((Layer_Para_Padding *)Layers->Layer_Para_Extra)->padding_bottom;//底部填充行
-    reg_t Padding_Left = ((Layer_Para_Padding *)Layers->Layer_Para_Extra)->padding_left;//左边填充行
-    reg_t Padding_Right = ((Layer_Para_Padding *)Layers->Layer_Para_Extra)->padding_right;//右边填充行
+    reg_t Padding = ((Layer_Para_Padding *)Layers->Layer_Para_Extra)->padding;//填充几圈0
 #if (CHW==1)
     //在此实现CHW的内存访问逻辑
     for (int i = 0; i < Output_Fmap_Channel; i++)//Output_Fmap_Channel
@@ -27,26 +24,26 @@ void OpenNNA_Operator_Padding(struct layer *Layers)
             for (int k = 0; k < Output_Fmap_Col; k++)//Output_Fmap_Col 列
             {
                 /*j遍历行,顶部和底部填充*/
-                if ( j < Padding_Top)//顶部padding
+                if ( j < Padding)//顶部padding
                 {
-                    ((data_t *)Layers->Output_Feature_Map)[k+Output_Fmap_Col*j+Output_Fmap_Col*Output_Fmap_Row*i] = 0;
+                    ((data_t *)Layers->Output_Feature_Map)[k+(Output_Fmap_Col*j)+(Output_Fmap_Col*Output_Fmap_Row*i)] = 0;
                 }
-                else if ((j >= (Padding_Top + Input_Fmap_Row))&&(j <= (Padding_Top + Padding_Bottom + Input_Fmap_Row)))//底部padding
+                else if (j >= (Padding + Input_Fmap_Row))//底部padding
                 {
-                    ((data_t *)Layers->Output_Feature_Map)[k+Output_Fmap_Col*j+Output_Fmap_Col*Output_Fmap_Row*i] = 0;
+                    ((data_t *)Layers->Output_Feature_Map)[k+(Output_Fmap_Col*j)+(Output_Fmap_Col*Output_Fmap_Row*i)] = 0;
                 }
                 /*k遍历列,左边和右边填充*/
-                if (k < Padding_Left)//左边padding
+                if (k < Padding)//左边padding
                 {
-                    ((data_t *)Layers->Output_Feature_Map)[k+Output_Fmap_Col*j+Output_Fmap_Col*Output_Fmap_Row*i] = 0;
+                    ((data_t *)Layers->Output_Feature_Map)[k+(Output_Fmap_Col*j)+(Output_Fmap_Col*Output_Fmap_Row*i)] = 0;
                 }
-                else if ((k >= (Padding_Left + Input_Fmap_Col))&&(k <= (Padding_Left + Padding_Right + Input_Fmap_Col)))//右边padding
+                else if (k >= (Padding + Input_Fmap_Col))//右边padding
                 {
-                    ((data_t *)Layers->Output_Feature_Map)[k+Output_Fmap_Col*j+Output_Fmap_Col*Output_Fmap_Row*i] = 0;
+                    ((data_t *)Layers->Output_Feature_Map)[k+(Output_Fmap_Col*j)+(Output_Fmap_Col*Output_Fmap_Row*i)] = 0;
                 }
                 //输入fmap填充到中间
-                if(j >= Padding_Top && j < (Padding_Top + Input_Fmap_Row) && k >= Padding_Left && k < (Padding_Left + Input_Fmap_Col))
-                    ((data_t *)Layers->Output_Feature_Map)[(i * Output_Fmap_Row * Output_Fmap_Col) + (j * Output_Fmap_Col) + k] = ((data_t *)Layers->Input_Feature_Map)[(i * Input_Fmap_Row * Input_Fmap_Col) + ((j-Padding_Top) * Input_Fmap_Col) + (k-Padding_Left)];
+                if(j >= Padding && j < (Padding + Input_Fmap_Row) && k >= Padding && k < (Padding + Input_Fmap_Col))
+                    ((data_t *)Layers->Output_Feature_Map)[k+(Output_Fmap_Col*j)+(Output_Fmap_Col*Output_Fmap_Row*i)]  = ((data_t *)Layers->Input_Feature_Map)[(i * Input_Fmap_Row * Input_Fmap_Col) + ((j-Padding) * Input_Fmap_Col) + (k-Padding)];
             }
         }
     }
--- src/operator/opennna_operator_padding.h
+++ src/operator/opennna_operator_padding.h
@@ -13,10 +13,7 @@
 struct Layer_Para_Padding{
     //在这里可以添加其他的层属性(每个算子独有)
     //例如卷积核的大小，stride等
-    unsigned int padding_top;//顶部填充几行0
-    unsigned int padding_bottom;//底部填充几行0
-    unsigned int padding_left;//左边填充几行0
-    unsigned int padding_right;//右边填充几行0
+    unsigned int padding;//填充几圈0
 };
 typedef struct Layer_Para_Padding Layer_Para_Padding;
 
