dc9d024c12764425aa6e95f3250279d23f38149e
Chengsen Dong <1034029664@qq.com>
2022-08-02 12:56:59 +0100

    upload stm32 port opennna

---
+++ platform/OpenNNA_STM32H7A3/Middlewares/OpenNNA2.0/operator/opennna_operator_padding.h
@@ -0,0 +1,23 @@
+//
+// Created by 董程森 on 2022/7/26.
+//
+
+#ifndef CORE_OPENNNA_OPERATOR_PADDING_H
+#define CORE_OPENNNA_OPERATOR_PADDING_H
+
+#include "opennna_core.h"
+
+
+
+//层参数
+struct Layer_Para_Padding{
+    //在这里可以添加其他的层属性(每个算子独有)
+    //例如卷积核的大小，stride等
+    unsigned int padding;//填充几圈0
+};
+typedef struct Layer_Para_Padding Layer_Para_Padding;
+
+//算子
+void OpenNNA_Operator_Padding(struct layer *Layers);
+
+#endif //CORE_OPENNNA_OPERATOR_PADDING_H
