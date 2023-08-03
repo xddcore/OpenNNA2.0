d9f36c039eb208b8e82e2537a4baccf9a28bd096
xddcore <1034029664@qq.com>
2023-08-03 10:47:22 +0000

    upload zero linux board demo

---
+++ platform/zero_linux_board/build/CMakeFiles/core.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_core.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_core.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_demo_mobilenet_0.25_128_int8.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_demo_mobilenet_0.25_128_int8.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_avgpool.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_avgpool.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_conv2d.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_conv2d.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_dense.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_dense.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_dwconv2d.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_dwconv2d.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_example.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_example.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_leakyrelu.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_leakyrelu.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_maxpool.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_maxpool.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_padding.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_padding.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_relu.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_relu.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_relu6.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_relu6.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_softmax.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_softmax.c.o"
+  "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/opennna_operator_tanh.c" "/xddcore_toolbox/OpenNNA2.0/platform/zero_linux_board/build/CMakeFiles/core.dir/opennna_operator_tanh.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././weights_bias_h"
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ platform/zero_linux_board/build/CMakeFiles/core.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/core.dir/opennna_core.c.o"
+  "CMakeFiles/core.dir/opennna_operator_example.c.o"
+  "CMakeFiles/core.dir/opennna_operator_dense.c.o"
+  "CMakeFiles/core.dir/opennna_operator_relu.c.o"
+  "CMakeFiles/core.dir/opennna_operator_softmax.c.o"
+  "CMakeFiles/core.dir/opennna_operator_conv2d.c.o"
+  "CMakeFiles/core.dir/opennna_operator_padding.c.o"
+  "CMakeFiles/core.dir/opennna_operator_maxpool.c.o"
+  "CMakeFiles/core.dir/opennna_operator_avgpool.c.o"
+  "CMakeFiles/core.dir/opennna_operator_relu6.c.o"
+  "CMakeFiles/core.dir/opennna_operator_dwconv2d.c.o"
+  "CMakeFiles/core.dir/opennna_operator_leakyrelu.c.o"
+  "CMakeFiles/core.dir/opennna_operator_tanh.c.o"
+  "CMakeFiles/core.dir/opennna_demo_mobilenet_0.25_128_int8.c.o"
+  "core.pdb"
+  "core"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
